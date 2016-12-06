f9b0e36ea487403d2a526dadb8322eba5cf45435
Minimalistic <minimalistic.public@gmail.com>
2016-12-05 20:25:00 -0600

    Signup functionality partially added
    Have some rudimentary signup functioning, still need to make it
    pervasive through entire website as well as needs hashing of passwords
    etc.

--- marshproject.py
+++ marshproject.py
@@ -8,7 +8,9 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 								autoescape = True)
 
-class SteveHandler(webapp2.RequestHandler):
+"""ADD DOCUMENTATION describing SteveBlogHandler """
+
+class StevesBlogHandler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
 
@@ -19,28 +21,91 @@ class SteveHandler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
+""" Form submittion processing functions provided by Udacity """
 
-class MainPage(webapp2.RequestHandler):
-	def get(self):
-		self.response.out.write(form)
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
 
-class Base(SteveHandler):
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Base(StevesBlogHandler):
 	def get(self):
 		self.render("base.html")
 
-class Blog(SteveHandler):
+class BlogFront(StevesBlogHandler):
 	def get(self):
 		self.render("blog.html")
 
-class SignUp(SteveHandler):
+class SignUp(StevesBlogHandler):
+
 	def get(self):
 		self.render("signup.html")
 
-class NewPost(SteveHandler):
+	def post(self):
+		have_error = False
+		username = self.request.get('username')
+		password = self.request.get('password')
+		verify = self.request.get('verify')
+		email = self.request.get('email')
+
+		params = dict(username = username,
+						email = email)
+
+		if not valid_username(username):
+			params['error_username'] = "That's not a valid username."
+			have_error = True
+
+		if not valid_password(password):
+			params['error_password'] = "That isn't a valid password."
+			have_error = True
+		elif password != verify:
+			params['error_verify'] = "Passwords entered were not identical."
+			have_error = True
+
+		if not valid_email(email):
+			params['error_email'] = "That's not a valid email."
+			have_error = True
+
+		if have_error:
+			self.render('signup.html', **params)
+		else:
+			self.redirect('/blog/welcome?username=' + username)
+
+class Welcome(StevesBlogHandler):
+	def get(self):
+		username = self.request.get('username')
+		if valid_username(username):
+			self.render('welcome.html', username = username)
+		else:
+			self.redirect('/signup')
+
+class Login(StevesBlogHandler):
+	def get(self):
+		self.render("login.html")
+
+class Logout(StevesBlogHandler):
+	def get(self):
+		self.render("logout.html")
+
+class NewPost(StevesBlogHandler):
 	def get(self):
 		self.render("newpost.html")
 
 app = webapp2.WSGIApplication([('/', Base),
-								('/blog', Blog),
-								('/blog/signup', SignUp),
-								('/blog/newpost', NewPost)], debug=True)
+								('/blog/?', BlogFront),
+								('/signup', SignUp),
+
+								('/login', Login),
+								('/logout', Logout),
+
+								('/blog/newpost', NewPost),
+
+								('/blog/welcome', Welcome),
+								], debug=True)
