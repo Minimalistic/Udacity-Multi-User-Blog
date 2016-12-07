7e18a406d4540103770198124d34cd14f543e843
Minimalistic <minimalistic.public@gmail.com>
2016-12-07 11:13:55 -0600

    Blog posting functional
    Added blog post functionality, still does not have user login
    pervasiveness/unable to comment nor delete posts.

--- marshproject.py
+++ marshproject.py
@@ -4,9 +4,15 @@ import re
 
 import jinja2
 
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 								autoescape = True)
+
+def render_str(template, **params):
+	t = jinja_env.get_template(template)
+	return t.render(params)
 
 """ADD DOCUMENTATION describing StevesBlogHandler """
 
@@ -15,11 +21,14 @@ class StevesBlogHandler(webapp2.RequestHandler):
 		self.response.out.write(*a, **kw)
 
 	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
+		return render_str(template, **params)
 
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
+
+def render_post(response, post):
+	response.out.write('<br>' + post.subject + '</b><br>')
+	response.out.write(post.content)
 
 """ Form submission processing functions provided by Udacity """
 
@@ -39,9 +48,34 @@ class Base(StevesBlogHandler):
 	def get(self):
 		self.render("base.html")
 
+def blog_key(name = 'default'):
+	return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
 class BlogFront(StevesBlogHandler):
-	def get(self):
-		self.render("blog.html")
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('blog.html', posts = posts)
+
+class PostPage(StevesBlogHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
 
 class SignUp(StevesBlogHandler):
 
@@ -98,6 +132,18 @@ class NewPost(StevesBlogHandler):
 	def get(self):
 		self.render("newpost.html")
 
+	def post(self):
+		subject = self.request.get('subject')
+		content = self.request.get('content')
+
+		if subject and content:
+			p = Post(parent = blog_key(), subject = subject, content = content)
+			p.put()
+			self.redirect('/blog/%s' % str(p.key().id()))
+		else:
+			error = "subject and content, please!"
+			self.render("newpost.html", subject=subject, content=content, error=error)
+
 app = webapp2.WSGIApplication([('/', Base),
 								('/blog/?', BlogFront),
 								('/signup', SignUp),
@@ -105,6 +151,7 @@ app = webapp2.WSGIApplication([('/', Base),
 								('/login', Login),
 								('/logout', Logout),
 
+								('/blog/([0-9]+)', PostPage),
 								('/blog/newpost', NewPost),
 
 								('/blog/welcome', Welcome),
