af95ac70de07ef06a7d3bfd98e42ade5c0335592
Minimalistic <minimalistic.public@gmail.com>
2016-12-13 11:41:15 -0600

    Functionally able to render specific post's content in html on editpost.html

--- marshblog.py
+++ marshblog.py
@@ -136,29 +136,6 @@ class BlogFront(BlogHandler):
         posts = greetings = Post.all().order('-created')
         self.render('blog.html', posts = posts)
 
-class NewPost(BlogHandler):
-    def get(self):
-        if self.user:
-            self.render("newpost.html")
-        else:
-            self.redirect("/login")
-
-    def post(self):
-        if not self.user:
-            self.redirect('/blog')
-
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-        author = self.user.name
-
-        if subject and content:
-            p = Post(parent = blog_key(), subject = subject, content = content, author = author)
-            p.put()
-            self.redirect('/blog/%s' % str(p.key().id()))
-        else:
-            error = "subject and content, please!"
-            self.render("newpost.html", subject = subject, content = content, error = error, author = author)
-
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
@@ -210,6 +187,29 @@ class EditPost(BlogHandler):
 			else:
 				error = "subject and content, please!"
 				self.render("editpost.html", subject=subject, content=content, error=error)
+				
+class NewPost(BlogHandler):
+    def get(self):
+        if self.user:
+            self.render("newpost.html")
+        else:
+            self.redirect("/login")
+
+    def post(self):
+        if not self.user:
+            self.redirect('/blog')
+
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+        author = self.user.name
+
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content, author = author)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "subject and content, please!"
+            self.render("newpost.html", subject = subject, content = content, error = error, author = author)
 
 class Signup(BlogHandler):
     def get(self):
