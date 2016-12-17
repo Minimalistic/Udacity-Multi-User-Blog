98a15933d31dca17fb4132f997fc68b995793efb
Minimalistic <minimalistic.public@gmail.com>
2016-12-17 17:43:17 -0600

    Separate models.py for models as well as a functional .gitignore
    Broke the glob of mongo-code of the blog up and put the models in their
    own file.

--- marshblog.py
+++ marshblog.py
@@ -1,32 +1,7 @@
-import os
 import re
-import random
-import hashlib
-import hmac
-from string import letters
+from models import *
 
 import webapp2
-import jinja2
-
-from google.appengine.ext import db
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
-
-secret = 'TheMostSecretOfSecrets'
-
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
-
-def make_secure_val(val):
-    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
-
-def check_secure_val(secure_val):
-    val = secure_val.split('|')[0]
-    if secure_val == make_secure_val(val):
-        return val
 
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -67,72 +42,6 @@ def render_post(response, post_tool):
 class MainPage(BlogHandler): # This renders the base.html if user goes to 
     def get(self):           # the root address
         self.render('base.html')
-
-##### user stuff
-def make_salt(length = 5):
-    return ''.join(random.choice(letters) for x in xrange(length))
-
-def make_pw_hash(name, pw, salt = None):
-    if not salt:
-        salt = make_salt()
-    h = hashlib.sha256(name + pw + salt).hexdigest()
-    return '%s,%s' % (salt, h)
-
-def valid_pw(name, password, h):
-    salt = h.split(',')[0]
-    return h == make_pw_hash(name, password, salt)
-
-def users_key(group = 'default'):
-    return db.Key.from_path('users', group)
-
-class User(db.Model):
-    name = db.StringProperty(required = True)
-    pw_hash = db.StringProperty(required = True)
-    email = db.StringProperty()
-
-    @classmethod
-    def by_id(cls, uid):
-        return User.get_by_id(uid, parent = users_key())
-
-    @classmethod
-    def by_name(cls, name):
-        u = User.all().filter('name =', name).get()
-        return u
-
-    @classmethod
-    def register(cls, name, pw, email = None):
-        pw_hash = make_pw_hash(name, pw)
-        return User(parent = users_key(),
-                    name = name,
-                    pw_hash = pw_hash,
-                    email = email)
-
-    @classmethod
-    def login(cls, name, pw):
-        u = cls.by_name(name)
-        if u and valid_pw(name, pw, u.pw_hash):
-            return u
-
-
-##### blog stuff
-
-def blog_key(name = 'default'):
-    return db.Key.from_path('blogs', name)
-
-class PostDatabase(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
-    user_id = db.IntegerProperty(required = True)
-
-    def fetchUserName(self):
-        user = User.by_id(self.user_id)
-        return user.name
-
-    def render(self): # renderpost is used to render jinja templates
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", renderpost = self)
 
 class BlogFront(BlogHandler):
     def get(self):
@@ -309,12 +218,6 @@ class Delete(BlogHandler):
 
         else:
             self.redirect('/blog')
-
-class Comment(db.Model):
-    comment = db.StringProperty(required=True)
-    post = db.ReferenceProperty(PostDatabase)
-    user = db.ReferenceProperty(User)
-
 
 class NewComment(BlogHandler):
 
---
+++ models.py
@@ -0,0 +1,95 @@
+import os
+import hmac
+import random
+from string import letters
+import hashlib
+
+from google.appengine.ext import db
+
+import jinja2
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
+secret = 'TheMostSecretOfSecrets'
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
+def users_key(group = 'default'):
+    return db.Key.from_path('users', group)
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+class PostDatabase(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+    user_id = db.IntegerProperty(required = True)
+
+    def fetchUserName(self):
+        user = User.by_id(self.user_id)
+        return user.name
+
+    def render(self): # renderpost is used to render jinja templates
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", renderpost = self)
+
+class Comment(db.Model):
+    comment = db.StringProperty(required=True)
+    post = db.ReferenceProperty(PostDatabase)
+    user = db.ReferenceProperty(User)
+
+
