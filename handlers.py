eca73445587d6e67d1ece7d2c354fbf1bebb5012
Minimalistic <minimalistic.public@gmail.com>
2016-12-29 09:20:01 -0600

    Re-converging models and user auth in to a main python file

--- handlers.py
+++ handlers.py
@@ -1,9 +1,143 @@
+import os
+import re
+import jinja2
+import hmac
+import hashlib
+import random
 import webapp2
 import time
 
-
+from string import letters
 from models import *
-from userAuth import *
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+secret = 'TheMostSecretOfSecrets'
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+
+
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
+PASS_RE = re.compile(r"^.{3,20}$")
+
+
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+# Cookie hashing
+
+
+def blog_key(name='default'):
+    return db.Key.from_path('blogs', name)
+
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+# Password hashing
+
+
+def make_salt(length=5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
+# Grouping key for users
+
+def users_key(group='default'):
+    return db.Key.from_path('users', group)
+
+class PostDatabase(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+    user_id = db.IntegerProperty(required=True)
+    likes = db.IntegerProperty(required=False)
+    liked_by = db.ListProperty(int)
+
+    def fetchUserName(self):
+        user = User.by_id(self.user_id)
+        return user.name
+
+    def render(self):  # renderpost is used to render jinja templates
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", renderpost=self)
+
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+
+class User(db.Model):
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent=users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email=None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent=users_key(),
+                    name=name,
+                    pw_hash=pw_hash,
+                    email=email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
+
+class Comment(db.Model):
+    comment_content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+    comment_owner = db.IntegerProperty(required=True)
+    comment_owner_text = db.TextProperty(required=True)
+
+    def getUserName(self):
+        user = User.by_id(self.comment_owner)
+        return user.name
 
 
 class BlogHandler(webapp2.RequestHandler):
