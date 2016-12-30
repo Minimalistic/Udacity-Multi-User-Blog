76d689a1691c775a9232df230748e92d50142148
Minimalistic <minimalistic.public@gmail.com>
2016-12-30 15:30:41 -0600

    Password hashing/salting/authorization mainly functional

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
 
 # Strings
 THE_SECRET = "TheSecretIs42"
-user_error_str = "Not a valid username."
-password_error_str = "Not a valid password."
-passwordconfirm_error_str = "Passwords do not match."
-email_error_str = "Not a valid email."
+user_err_string = "Not a valid username."
+pass_err_string = "Not a valid password."
+pass2_err_string = "Passwords do not match."
+email_err_string = "Not a valid email."
 
 
 def blog_key(name='default'):
@@ -56,7 +56,8 @@ def checkPass2(pass1, pass2):
 
 def checkEmail(email):
     email2 = re.compile(r"^[\S]+@[\S]+.[\S]+$")
-    return email2.match(email) or email == ""
+    return email2.match(email) or email ==""
+
 
 # Cookie related
 
@@ -81,20 +82,20 @@ def make_salt():
     return "".join(random.choice(string.letters) for x in xrange(5))
 
 
-def make_password_hash(name, password, salt=make_salt()):
-    h = hashlib.sha256(name + password + salt).hexdigest()
-    return '%s|%s' % (h, salt)
+def make_pw_hash(name, pw, salt=make_salt()):
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (h, salt)
 
 
-def valid_password(name, password, h):
+def valid_pw(name, pw, h):
     # This compares input password with the input confirmation password
     split = h.split(",")
     hash = split[0]
     salt = split[1]
 
     logging.warning("h =" + h)
-    logging.warning("make = " + make_password_hash(name, password, salt))
-    return h == make_password_hash(name, password, salt)
+    logging.warning("make = " + make_pw_hash(name, pw, salt))
+    return h == make_pw_hash(name, pw, salt)
 
 # DATABASES ##################################################################
 
@@ -105,6 +106,7 @@ class User(db.Model):
     username = db.StringProperty(required=True)
     password = db.StringProperty(required=True)
     email = db.StringProperty(required=False)
+    created = db.DateTimeProperty(auto_now_add=True)
 
 # Create a database for blog post information
 
@@ -113,13 +115,9 @@ class PostDatabase(db.Model):
     title = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
+    edited = db.DateTimeProperty(auto_now=True)
     user = db.StringProperty(required=True)
-    # likes = db.StringDatabase(required=True)
-
-    def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", renderpost=self)
+    likes = db.IntegerProperty(default=0)
 
 # Create a database for comment information
 
@@ -134,24 +132,28 @@ class Comment(db.Model):
 
 
 class BlogHandler(webapp2.RequestHandler):
+    # Checks if the user is logged in
+    def isLogged(self):
+        cookie = self.request.cookies.get('username')
+        if cookie and check_secure_val(cookie):
+            return cookie.split("|")[0]
+
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def isLogged(self):
-                cookie = self.request.cookies.get('username')
-                if cookie and check_secure_val(cookie):
-                                                return cookie.split("|")[0]
-
     def render_str(self, template, **params):
-        return render_str(template, **params)
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+    # Sets the cookie for login and redirects to welcome page
     def login(self, user):
-            val = make_secure_val(str(user))
-            self.response.headers.add_header("Set-Cookie", r"username=%s; Path=/" %val)
-            self.redirect("/welcome")
+        val = make_secure_val(str(user))
+        self.response.headers.add_header("Set-Cookie",
+                                         r"username=%s; Path=/" % val)
+        self.redirect("/welcome")
 
 
 class MainPage(BlogHandler):
@@ -164,6 +166,136 @@ class MainPage(BlogHandler):
         self.render('blog.html',        # takes the articles db query and
                     isLogged=isLogged,  # renders results
                     articles=articles)
+
+
+# Sign up page handler, shows a sign up form if user is not logged in
+class SignUpHandler(BlogHandler):
+    def get(self):
+        if(self.isLogged()):
+            self.redirect("/")
+        else:
+            self.render("signup.html",
+                        user_error="",
+                        pass_error="",
+                        pass2_error="",
+                        email_error="")
+
+    def post(self):
+        user = self.request.get("username")
+        pass1 = self.request.get("password")
+        pass2 = self.request.get("verify")
+        email = self.request.get("email")
+
+        # Checking every field
+        user_ok = checkUser(user)
+        pass_ok = checkPass(pass1)
+        pass2_ok = checkPass2(pass1, pass2)
+        email_ok = checkEmail(email)
+
+        # Checking if all fields are ok
+        if(user_ok and pass_ok and pass2_ok and email_ok):
+
+            # Checking if the user already exists
+            existing_user = db.GqlQuery("""SELECT * FROM User
+                                        WHERE username=\'""" +
+                                        user + "\'").get()
+            if(existing_user):
+                user_error = "There already is a user with that name."
+                self.render("signup.html",
+                            user_error=user_error,
+                            email=email)
+                return
+            else:
+                # Signing up
+                salt = make_salt()
+                password = make_pw_hash(THE_SECRET, pass1, salt)
+                if(email):
+                    user_to_be_added = User(username=user,
+                                            password=password,
+                                            email=email)
+                else:
+                    user_to_be_added = User(username=user,
+                                            password=password)
+                db.put(user_to_be_added)
+                self.login(user)
+
+        # Initialising / setting error messages
+        if user_ok:
+            user_error = ""
+        else:
+            user_error = user_err_string
+            user = ""
+
+        if pass_ok:
+            if pass2_ok:
+                pass2_error = ""
+            else:
+                pass2_error = pass2_err_string
+            pass_error = ""
+        else:
+            pass_error = pass_err_string
+            pass2_error = ""
+
+        if email_ok:
+            email_error = ""
+        else:
+            email_error = email_err_string
+            email = ""
+
+        self.render("signup.html",
+                    user_error=user_error,
+                    pass_error=pass_error,
+                    pass2_error=pass2_error,
+                    email_error=email_error,
+                    user=user,
+                    email=email)
+
+
+# Log in handler, shows a form if the user is not logged in
+class LoginHandler(BlogHandler):
+    def get(self):
+        if(self.isLogged()):
+            self.render("error.html",
+                        error="You are already logged in!")
+        else:
+            self.render("login.html")
+
+    def post(self):
+        user = self.request.get("username")
+        password = self.request.get("password")
+
+        # Checking if both fields are not empty
+        if user and password:
+
+            # Checking if the username and password match
+            db_users = db.GqlQuery("SELECT * FROM User WHERE username=\'" + user + "\'")
+            if(db_users.get()):
+                db_user = db_users[0]
+                if valid_pw(THE_SECRET, password, db_user.password):
+                    self.login(user)
+                else:
+                    self.render("login.html",
+                                user=user,
+                                error="Username and password do not match.")
+            else:  # If the user does not exist.
+                self.render("login.html",
+                            user=user,
+                            error="Username and password do not match.")
+        else:
+            self.render("login.html",
+                        user=user,
+                        error="A username and password are required.")
+
+
+class WelcomeHandler(BlogHandler):
+    def get(self):
+        username = self.isLogged()
+        if username:
+            self.render("welcome.html",
+                        isLogged=True,
+                        username=username)
+        else:
+            self.redirect("/signup")
 
 
 class PostLinkHandler(BlogHandler):
@@ -341,14 +473,20 @@ class EditCommentHandler(BlogHandler):
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
+                               ("/signup", SignUpHandler),
+                               ("/login", LoginHandler),
+                               ("/welcome", WelcomeHandler),
                                ('/([0-9]+)', PostLinkHandler),
                                ('/newpost', NewPost),
                                ('/editpost/([0-9]+)', EditPost),
                                ('/delete/([0-9]+)', Delete),
                                ('/like/([0-9]+)', LikePost),
                                ('/success', Success),
-                               ('/([0-9]+)/addcomment/([0-9]+)', AddCommentHandler),
-                               ('/comment/delete/([0-9]+)/([0-9]+)', DeleteCommentHandler),
-                               ('/comment/edit/([0-9]+)/([0-9]+)', EditCommentHandler),
+                               ('/([0-9]+)/addcomment/([0-9]+)',
+                                AddCommentHandler),
+                               ('/comment/delete/([0-9]+)/([0-9]+)',
+                                DeleteCommentHandler),
+                               ('/comment/edit/([0-9]+)/([0-9]+)',
+                                EditCommentHandler),
                                ],
                               debug=True)
