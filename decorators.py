83e9d1c20ffc8941fb4625306611d472bd71c673
Minimalistic <minimalistic.public@gmail.com>
2017-01-17 11:55:22 -0600

    Revert "Rudimentary decorator implimented"
    This reverts commit 648f68432877d11d80bb7c6a213b7897768dc273.

--- decorators.py
+++
@@ -1,7 +0,0 @@
-from file_helpers import *
-
-
-def post_exists(function):
-    def _wrapper(self, post_id):
-        article = Article.get_by_id(int(id))
-    return _wrapper
--- handlers/post_handler.py
+++ handlers/post_handler.py
@@ -1,15 +1,13 @@
 from file_helpers import *
 from blog_handler import BlogHandler
 
-from decorators import post_exists
-
 
 class PostHandler(BlogHandler):
     """
     Handler that manages existing posts.
     """
-    @post_exists
     def get(self, id):
+        article = Article.get_by_id(int(id))
         comments = db.GqlQuery("SELECT * FROM Comment WHERE article_id=" +
                                str(int(id)) + " ORDER BY created DESC")
         if article is None:
